e7aa2e7c76cf80773f297a4b33054dee2fe31da4
samsonsuen <samsons2026@student.cis.edu.hk>
2023-11-14 14:19:40 +0800

    done

--- src/ArrayUtility.java
+++ src/ArrayUtility.java
@@ -1,74 +1,156 @@
 public class ArrayUtility {
 
     public static void print(int[] array) {
-
+            System.out.print(array);
     }
 
     public static int sum(int[] array) {
-        return 0;
+        int sum = 0;
+        for(int i = 0; i<array.length; i++){
+            sum = sum + array[i];
+        }
+        return sum;
     }
 
     public static double average(int[] array) {
-        return 0;
+        double sum = 0;
+        for(int i = 0; i<array.length; i++){
+            sum = sum + array[i];
+        }double average = sum/array.length;
+        int dp = (int)(average*100);
+        double output = (double)dp/100;
+        return output;
     }
 
     public static int minimum(int[] array) {
-        return 0;
+        int min = array[0];
+        for(int i = 0; i<array.length; i++){
+            if(array[i] < min){
+                min = array[i];
+            }
+        }
+        return min;
     }
 
     public static int maximum(int[] array) {
-        return 0;
+        int max = array[0];
+        for(int i = 0; i<array.length; i++){
+            if(array[i] > max){
+                max = array[i];
+            }
+        }
+        return max;
     }
 
+
     public static int evenCount(int[] array) {
-        return 0;
+        int count = 0;
+        for(int i = 0; i<array.length; i++){
+            if(array[i]%2==0){
+                count++;
+            }
+        }
+        return count;
     }
 
     public static int[] reverseOne(int[] array) {
-        return null;
+        int[] reversed = new int[array.length];
+        for(int i = array.length-1; i>=0; i--){
+            reversed[array.length-i-1]=array[i];
+        }
+        return reversed;
     }
 
     public static void reverseTwo(int[] array) {
-
+        for(int i = 0; i<array.length/2; i++){
+            int a = array[i];
+            array[i] = array[array.length-1-i];
+            array[array.length-1-i]=a;
+        }
     }
 
     public static boolean linearSearchInt(int[] array, int num) {
-        return false;
+        for(int i = 0; i<array.length; i++){
+            if(array[i]==num){
+                return true;
+            }
+        }return false;
     }
 
     public static boolean linearSearchString(String[] array, String str) {
-        return false;
+        for(int i = 0; i<array.length; i++){
+            if(array[i].equals(str)){
+                return true;
+            }
+        }return false;
     }
 
     public static void multiplyByTwo(int[] array) {
-
+        for(int i = 0; i<array.length; i++){
+            array[i]=array[i]*2;
+        }
     }
 
     public static void multiplyByN(int[] array, int n) {
-
+        for(int i = 0; i<array.length; i++){
+            array[i]=array[i]*n;
+        }
     }
 
     public static String toString(int[] array) {
-        return null;
+        String output = "";
+        for(int i = 0; i<array.length; i++){
+            if(i==array.length-1){
+                output = output + array[i];
+            }else{
+                output = output + array[i] +", ";
+            }
+        }return output;
     }
 
     public static boolean twoSum(int[] array, int num) {
+        for(int i=0;i<array.length;i++){
+            for(int j=0;j<array.length;j++){
+                if(i==j){
+                    continue;
+                }else if(array[i]+array[j]==num){
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
     public static void shiftRight(int[] array) {
-
+        int last = array[array.length-1];
+        for(int i = array.length-1; i>=1; i--){
+            array[i]=array[i-1];
+        }array[0]=last;
     }
 
     public static void shiftLeft(int[] array) {
-
+        int first = array[0];
+        for(int i = 0; i<array.length-1; i++){
+            array[i]=array[i+1];
+        }array[array.length-1]=first;
     }
 
     public static void shiftRightNTimes(int[] array, int n) {
-
+        for (int a = 0; a < n; a++) {
+            int last = array[array.length - 1];
+            for (int i = array.length - 1; i >= 1; i--) {
+                array[i] = array[i - 1];
+            }
+            array[0] = last;
+        }
     }
-
     public static void shiftLeftNTimes(int[] array, int n) {
+        for(int a = 0; a<n; a++){
+            int first = array[0];
+            for(int i = 0; i<array.length-1; i++){
+                array[i]=array[i+1];
+            }array[array.length-1]=first;
+        }
 
     }
 
